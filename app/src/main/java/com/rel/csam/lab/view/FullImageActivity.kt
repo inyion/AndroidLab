1a41ed0b3046162ce47b4c97c415252d91d26a48
iliy <iliy@qoo10.com>
2019-01-14 15:02:12 +0900

    풀스크린모드로 하면 줌인아웃이 깨짐

--- app/src/main/java/com/rel/csam/lab/view/FullImageActivity.kt
+++ app/src/main/java/com/rel/csam/lab/view/FullImageActivity.kt
@@ -2,7 +2,6 @@ package com.rel.csam.lab.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import com.bumptech.glide.Glide
 import com.rel.csam.lab.R
 import kotlinx.android.synthetic.main.activity_full_image.*
@@ -16,17 +15,6 @@ class FullImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_full_image)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        supportActionBar?.hide()
-        fullscreen_content.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-
         // Set up the user interaction to manually show or hide the system UI.
         Glide.with(this).load(intent.getStringExtra("image")).into(fullscreen_content)
     }
